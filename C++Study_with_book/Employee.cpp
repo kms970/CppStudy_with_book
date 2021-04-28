54d05c577c44eb4a8a7a3f698de38f28ba134696
kms970 <49391285+kms970@users.noreply.github.com>
2021-04-29 01:37:37 +0900

    Employee 클래스 완성

--- C++Study_with_book/Employee.cpp
+++ C++Study_with_book/Employee.cpp
@@ -13,4 +13,75 @@ namespace Records {
 	{
 		setSalary(getSalary() + raiseAmount);
 	}
+
+	void Employee::demote(int demeritAmount)
+	{
+		setSalary(getSalary() - demeritAmount);
+	}
+
+	void Employee::hire()
+	{
+		mHired = true;
+	}
+
+	void Employee::fire()
+	{
+		mHired = false;
+	}
+
+	void Employee::display()const
+	{
+		cout << "EmployeeName: " << getLastName() << "," << getFirstName() << endl;
+		cout << "-----------------------------" << endl;
+		cout << (isHired() ? "Current Employee" : "Former Employee") << endl;
+		cout << "EmployeeNumber: " << getEmployeeNumber() << endl;
+		cout << "EmployeeSalary: $" << getSalary() << endl;
+		cout << endl;
+	}
+
+	const string& Employee::getFirstName()const
+	{
+		return mFirstName;
+	}
+
+	void Employee::setFirstName(const string& firstName)
+	{
+		mFirstName = firstName;
+	}
+
+	const string& Employee::getLastName()const
+	{
+		return mLastName;
+	}
+
+	void Employee::setLastName(const string& lastName)
+	{
+		mLastName = lastName;
+	}
+
+	int Employee::getEmployeeNumber()const
+	{
+		return mEmployeeNumber;
+	}
+
+	void Employee::setEmployeeNumber(int employeeNumber)
+	{
+		mEmployeeNumber = employeeNumber;
+	}
+
+	int Employee::getSalary()const
+	{
+		return mSalary;
+	}
+
+	void Employee::setSalary(int newSalary)
+	{
+		mSalary = newSalary;
+	}
+
+	bool Employee::isHired()const
+	{
+		if (mHired) return true;
+		else return false;
+	}
 }
